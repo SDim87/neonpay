fba34bc292706ad33db69d5a0383a95a494afc7d
SDim87 <da_sorokin@mail.ru>
2022-07-02 18:44:26 +0300

    Added events user, favicons. Fix bugs

--- src/common.blocks/header/header.js
+++ src/common.blocks/header/header.js
@@ -1,51 +1,12 @@
-let isFixed = false
-const $header = $('.js-header')
-const $nextBlock = $('.js-header__order')
-const offset = 40 + $header.outerHeight(true)
-const $btnBurger = $('.js-header__btn')
-const $btnClose = $('.js-header__btn_close')
-const $bropdown = $('.js-header__dropdown')
 const $scrollLine = $('.js-header__horizont-line')
-const mod = 'is-opened'
+const $blockPay = $('.js-header__pay')
+const $blockFastBuy = $('.js-header__fast-buy')
 
-// Выпадающее шапка и добавление верхнего отступа
-$(window).scroll(function switchHeader() {
-  if ($(this).scrollTop() >= $header.offset().top + offset) {
-    $header.addClass('is-fixed')
-    $nextBlock.addClass('is-padded')
-    isFixed = true
-  }
 
-  if ($(this).scrollTop() <= $nextBlock.offset().top + offset && isFixed) {
-    $header.removeClass('is-fixed')
-    $nextBlock.removeClass('is-padded')
-    isFixed = false
-  }
-})
-
-// Открытие мобильного меню
-$btnBurger.click((evt) => {
-  evt.preventDefault()
-
-  if (!$btnBurger.hasClass(mod)) {
-    $header.addClass(mod)
-    $btnBurger.addClass(mod)
-    $btnClose.addClass(mod)
-    $bropdown.addClass(mod)
-  }
-})
-
-// Закрытие мобильного меню
-$btnClose.click((evt) => {
-  evt.preventDefault()
-
-  if ($btnClose.hasClass(mod)) {
-    $header.removeClass(mod)
-    $btnBurger.removeClass(mod)
-    $btnClose.removeClass(mod)
-    $bropdown.removeClass(mod)
-  }
-})
+// Возвращает высоту до блока сверху
+function getOffsetFromBlock(block) {
+  return Math.ceil(block.offset().top)
+}
 
 // Возвращает высоту скролл области
 function getDocumentHeight() {
@@ -62,14 +23,25 @@ function procentWidth() {
   return `${Math.round((getScrollOffset() / getDocumentHeight()) * 100)}%`
 }
 
+const pointHiddenBlock = () =>
+  Math.ceil(getOffsetFromBlock($blockPay))
+
 // Движение horizontal-line
 $(window).scroll(() => {
-  getDocumentHeight()
   getScrollOffset()
+  getDocumentHeight()
   procentWidth()
+  const coordHiddenBlock = pointHiddenBlock()
+
+  if (coordHiddenBlock <= getScrollOffset()) {
+    $blockFastBuy.addClass('is-none')
+  } else {
+    $blockFastBuy.removeClass('is-none')
+  }
 
   $(window).resize(() => {
     getDocumentHeight()
+    pointHiddenBlock()
   })
 
   $scrollLine.css('width', procentWidth())
